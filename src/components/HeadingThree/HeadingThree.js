1141da8aaa03d072d57a26148219e7b604e32031
Caio Balduino <caiobalduinodev@hotmail.com>
2024-09-17 10:33:45 -0300

    ✨ feat: Create title components

---
+++ src/components/HeadingThree/HeadingThree.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './style.css';
+
+const HeadingThree = ({ text }) => {
+  return <h3>{text}</h3>;
+};
+
+export default HeadingThree;
---
+++ src/components/SubTitle/SubTitle.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './style.css';
+
+const SubTitle = ({ text }) => {
+  return <h2>{text}</h2>;
+};
+
+export default SubTitle;
